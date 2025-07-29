50d1bc417be10556b6f891c6c85e8e4621919b3a
blackcarblack <uuuuu5wwwww5@gmail.com>
2025-07-29 17:10:58 +0200

    DOOM

--- Market/urls.py
+++ Market/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 from . import views
+from . import auth_views
 
 app_name = 'Market'
 
 urlpatterns = [
+    # Основные страницы
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('team/', views.team, name='team'),
+    
+    # Аутентификация
+    path('login/', auth_views.user_login, name='login'),
+    path('register/', auth_views.user_register, name='register'),
+    path('logout/', auth_views.user_logout, name='logout'),
+    path('profile/', auth_views.profile, name='profile'),
+    
+    # AJAX для CAPTCHA
+    path('get-captcha/', auth_views.get_captcha, name='get_captcha'),
 ]
