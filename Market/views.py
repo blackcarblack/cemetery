1d301a03fd8e250cb31abf7443c1649a286d53e4
liNDRof <@email.com>
2025-08-20 17:55:06 +0200

    working

--- Market/views.py
+++ Market/views.py
@@ -134,7 +134,7 @@ def add_to_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
 
-    return redirect('Market:menu')
+    return redirect('Market:view_cart')
 
 
 
@@ -164,9 +164,6 @@ def remove_from_cart(request, product_id):
             cart_item.delete()
 
     return redirect('user_cart')
-
-
-
 
 
 
